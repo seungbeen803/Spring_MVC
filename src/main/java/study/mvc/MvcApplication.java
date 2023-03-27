468c604e6f010a21e16dcfd3f0ab43a26573675e
seungbeen <seungbin803@gmail.com>
2023-03-27 12:20:27 +0900

    Initial commit

---
+++ src/main/java/study/mvc/MvcApplication.java
@@ -0,0 +1,13 @@
+package study.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcApplication {
+	public static void main(String[] args) {
+		System.out.println("Hello1234");
+		SpringApplication.run(MvcApplication.class, args);
+	}
+
+}
