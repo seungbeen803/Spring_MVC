8a0b00d703a7c46f82c3c97b64d470f8c806f2af
seungbeen <seungbin803@gmail.com>
2023-03-28 10:18:37 +0900

    3월 28일 MVC

--- src/main/java/study/mvc/MyController.java
+++ src/main/java/study/mvc/MyController.java
@@ -1,12 +1,110 @@
 package study.mvc;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.HashMap;
+
+
 @RestController
+@RequestMapping("/api")
 public class MyController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello";
+//    @GetMapping("/hello")
+//    public String hello() {
+//        return "Hello";
+//    }
+
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    // (request : 요청 메세지를 객체화, response : 응답 메세지를 객체화)
+    public void hello(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setStatus(HttpStatus.OK.value()); // 상수
+        response.setStatus(200);
+        response.setHeader("Content-Type", "text/plain; charset=utf-8");
+        response.setHeader("Content-length", "5");
+        response.getWriter().write("Hello");
+    }
+
+    @GetMapping("/echo")
+    public void echo(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 메서드 정보 접근
+        String method = request.getMethod();
+        System.out.println("Method : " + method);
+
+        // 주소 정보 접근
+        String uri = request.getRequestURI();
+        System.out.println("URI : " + uri);
+
+        // 쿼리 스트링 정보 접근
+        String query = request.getQueryString();
+        System.out.println("QueryString " + query);
+
+        // 프로토콜, HTTP 버전 정보 접근
+        String protocol = request.getProtocol();
+        System.out.println("Protocol : " + protocol);
+
+        // HashMap(복습)
+//        HashMap<String, String> map = new HashMap<>();
+//        String[] parts = query.split("&");
+//        for(String p : parts){
+//            String[] tmp = p.split("=");
+//            map.put(tmp[0], tmp[1]);
+//        }
+//        System.out.println(map);
+
+        // 헤더 정보 접근
+        System.out.println("Headers");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames != null && headerNames.hasMoreElements()) {
+            String h = headerNames.nextElement();
+            System.out.println(h + " : " + request.getHeader(h));
+        }
+
+        // 요청 메세지의 바디 데이터 접근
+        byte[] bytes = request.getInputStream().readAllBytes();
+        String bytesToString = new String(bytes, StandardCharsets.UTF_8);
+        System.out.println(bytesToString);
+
+        // 응답 헤더 설정
+        response.setHeader("Content-Type", "text/plain; charset=utf-8");
+        //전달받은 body 텍스트를 그대로 응답하도록 설정
+        response.getWriter().write(bytesToString);
+    }
+        @GetMapping("/hello-html")
+        public void helloHTML(HttpServletResponse response) throws IOException {
+//        response.setStatus(HttpStatus.OK.value()); // 상수
+            response.setStatus(200);
+            // 만약 해당 내용을 text/plain으로 바꾸면?
+            response.setHeader("Content-Type", "text/plain; charset=utf-8");
+            response.getWriter().write("<h1>Hello<h1>");
+    }
+
+    @GetMapping("/hello-xml")
+    public void helloXML(HttpServletResponse response) throws IOException {
+    // 상태 코드와 관련된 상수를 제공하므로 이용해도 무방함
+        response.setStatus(HttpStatus.OK.value());
+    // "text/xml"이 아님을 유의
+        response.setHeader("Content-Type", "application/xml; charset=utf-8");
+        response.getWriter().write("<text>Hello</text>");
+    }
+
+    @GetMapping("/hello-json")
+    public void helloJSON(HttpServletResponse response) throws IOException {
+    // 성공적으로 리소스를 찾아서 돌려주면서 404 코드를 돌려줘도, 스프링 쪽에서는 속사정을 알 방법이 없으니 허용하고 잘 동작함
+    // (리소스가 존재하지 않는 이유를 json 같은걸로 설명할 수도 있으므로, HTTP 스펙 상에서도 204와는 달리, 404 코드를 돌려줄 때
+    // 바디 데이터를 포함하지 않아야 된다고 명시하지 않았음. 단, 웹 브라우저에서는 404이므로 문제라고 인식함, 그렇다고 해도 4XX
+    // 에러에 대한 처리는 프로그래머가 해야 함)
+        response.setStatus(404);
+    // "text/json"이 아님을 유의
+        response.setHeader("Content-Type", "application/json");
+        response.getWriter().write("{ \"data\": \"Hello\" }");
     }
 }
