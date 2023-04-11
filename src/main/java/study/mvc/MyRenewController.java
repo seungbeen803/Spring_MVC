082b7bd9b45312491101e645a0b5c576bfdd8457
seungbeen <seungbin803@gmail.com>
2023-04-11 10:17:27 +0900

    4월 11일 2교시 MyController를 스프링다운 방식으로 다시 구현해보기 @PathVariable, @RequestParam, @RequestHeader 사용해서 postman으로 결과 출력해보기

--- src/main/java/study/mvc/MyRenewController.java
+++ src/main/java/study/mvc/MyRenewController.java
@@ -1,14 +1,17 @@
 package study.mvc;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/renew")
 public class MyRenewController {
+
     // produces 옵션을 통해서 미디어 타입 지정 가능 (유추해서 자동으로 지정하게 할 수도 있지만가급적 써주는 것을 권장)
     @GetMapping(value = "/echo", produces = MediaType.TEXT_PLAIN_VALUE)
     // 반환한 문자열이 바로 응답 메시지의 바디 데이터에 삽입될 수 있도록 @ResponseBody 어노테이션 추가 (@RestController를 사용하면 생략 가능)
@@ -24,6 +27,67 @@ public class MyRenewController {
         System.out.println(bytesToString);
     // Content-Type 헤더의 경우 produces 옵션을 제공하여 미디어 타입 지정 가능
         return bytesToString;
+    }
+
+    // "/hello-html"
+    @GetMapping(value = "/hello-html", produces = MediaType.TEXT_HTML_VALUE)
+    // 반환 코드도 마찬가지로 그냥 성공적으로 메서드에서 값을 반환하면 자동으로 200이 됨
+    // @ResponseStatus를 지워도 출력
+    @ResponseStatus(HttpStatus.OK)
+    public String helloHTML() {
+        return "<h1>Hello</h1>";
+    }
+
+    @GetMapping(value = "/hello-xml", produces = MediaType.TEXT_XML_VALUE)
+    // 생략가능
+    @ResponseStatus(HttpStatus.OK)
+    public String helloXML() {
+        return "<text>Hello</text>";
+    }
+
+    @GetMapping(value = "/hello-json", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public String helloJSON() {
+        return "{ \"data\": \"Hello\" }";
+    }
+
+    // @RequestHeader 사용
+//    @GetMapping(value = "/echo-repeat", produces = MediaType.TEXT_PLAIN_VALUE)
+//    // @RequestHeader 어노테이션을 통해서 X-Repeat-Count에 적힌 숫자 정보 가져오고 없으면 1로 초기화
+//    public String echoRepeat(@RequestParam("word") String word, @RequestHeader(value
+//            = "X-Repeat-Count", defaultValue = "1") Integer repeatCount) throws IOException
+//    {
+//        String result = "";
+//        for(int i=0;i<repeatCount;i++) {
+//            result += word;
+//        }
+//        return result;
+//    }
+
+    // @PathVariable 사용
+    @GetMapping(value = "/echo-repeat/{repeatCount}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String echoRepeat2(
+            @PathVariable(value = "repeatCount", required = true) Integer repeatCount,
+            @RequestParam("word") String word) throws IOException
+    {
+        String result = "";
+        for(int i=0;i<repeatCount;i++) {
+            result += word;
+        }
+        return result;
+    }
+
+    // @RequestParam 추가
+    @GetMapping(value = "/echo-repeat", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String echoRepeat3(
+            @RequestParam("word") String word,
+            @RequestParam("repeatCount") Integer repeatCount) throws IOException
+    {
+        String result = "";
+        for(int i=0;i<repeatCount;i++) {
+            result += word;
+        }
+        return result;
     }
 
 }
